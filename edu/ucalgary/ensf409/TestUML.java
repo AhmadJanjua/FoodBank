1dd5d371a826a4ad74b970463827c85b95bbc028
Orionahn <101444254+Orionahn@users.noreply.github.com>
2022-04-01 14:56:49 -0600

    Added Order+OrderForm+Delivery Testing

--- edu/ucalgary/ensf409/TestUML.java
+++ edu/ucalgary/ensf409/TestUML.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 
 /**
  * ENSF 409 Final Project Group 7
- * @author Ahmad Janjua
- * @version 1.0
+ * @author Ahmad Janjua, Pedro Ghodsi
+ * @version 1.1
  * @since 1.0
  */
 
@@ -296,5 +296,98 @@ public class TestUML {
         expected += "3       Protein\n";
         expected += "2       Other\n";
         assertEquals("The outputted format didnt match the expected value",expected, hamper.createOrderFormat());
+    }
+    
+    // Test Delivery, Order, Order Format
+    String OUTPUT_FILENAME = "orderout.txt";
+
+    Delivery testDelivery;
+    Hamper testHamper;
+    Order testOrder;
+    OrderFormat testOrderFormat;
+    
+    
+    // setup methods beofe testing
+    @Before
+    public void setup() {
+        testHamper = new Hamper();
+        testOrder = new Order();
+        testOrderFormat = new OrderFormat();
+
+        // Delete the orderout file
+        File orderoutFile = new File(OUTPUT_FILENAME);
+        orderoutFile.delete();
+    }
+    
+    @Test
+    public void testGetDeliveryId() {
+        int expectedDeliveryId = 1;
+        testDelivery = new Delivery(expectedDeliveryId, 0);
+        assertEquals(expectedDeliveryId, testDelivery.getDeliveryId());
+    }
+
+    @Test
+    public void testGetDeliveryTime() {
+        int expectedDeliveryTime = 1;
+        testDelivery = new Delivery(0, expectedDeliveryTime);
+        assertEquals(expectedDeliveryTime, testDelivery.getDeliveryTime());
+    }
+
+    @Test
+    public void testSetDeliveryId() {
+        int expectedDeliveryId = 1;
+        testDelivery = new Delivery(0, 0);
+        testDelivery.setDeliveryId(expectedDeliveryId);
+        assertEquals(expectedDeliveryId, testDelivery.getDeliveryId());
+    }
+
+    @Test
+    public void testSetDeliveryTime() {
+        int expectedDeliveryTime = 1;
+        testDelivery = new Delivery(0, 0);
+        testDelivery.setDeliveryTime(expectedDeliveryTime);
+        assertEquals(expectedDeliveryTime, testDelivery.getDeliveryTime());
+    }
+
+    @Test
+    public void testCreateOrderForm() {
+        testOrderFormat.createOrderForm();
+        File file = new File(OUTPUT_FILENAME);
+
+        assertTrue(file.exists());
+    }
+
+    @Test
+    public void testUpdateStock() {
+        testOrderFormat.updateStock();
+
+        // Unable to assert test yet, further information about SQL needed
+    }
+
+    @Test
+    public void testCanComplete() {
+        Hamper newHamper = new Hamper(new ArrayList<Integer>() {{ add(1); }});
+        testOrder.addHamper(newHamper);
+        assertTrue(testOrder.canComplete());
+    }
+
+    @Test
+    public void testAddHamper() {
+        int expectedHamperSize = 1;
+        Hamper newHamper = new Hamper(new ArrayList<Integer>() {{ add(1); }});
+        testOrder.addHamper(newHamper);
+        assertEquals(expectedHamperSize, testOrder.getHampers().size());
+    }
+
+    @Test
+    public void testResetOrder() {
+        // Unable to assert test yet
+    }
+
+    @Test
+    public void testGetHampers() {
+        Hamper newHamper = new Hamper(new ArrayList<Integer>() {{ add(1); }});
+        testOrder.addHamper(newHamper);
+        assertEquals(new ArrayList<Hamper>() {{ add(newHamper); }}, testOrder.getHampers());
     }
 }
