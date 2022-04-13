af2dbda83f58ced654a5e0ecb2c0d641c6c5d3d0
AhmadJanjua <100788678+AhmadJanjua@users.noreply.github.com>
2022-04-13 13:26:35 -0600

    Update FoodList.java

--- edu/ucalgary/ensf409/FoodList.java
+++ edu/ucalgary/ensf409/FoodList.java
@@ -1,16 +1,62 @@
 package edu.ucalgary.ensf409;
 
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class FoodList {
-    
-    private ArrayList<Food> currentFood;
 
-    public FoodList(Food food){}
+    public static void main(String[] args) {
+        FoodList fl = new FoodList();
+        Iterator<Food> it = fl.getFoodList().iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next().getNAME());
+          }
+        fl.close();
+    }
 
-    public FoodList(ArrayList<Food> foodList) throws IllegalArgumentException{}
 
-    public void addFood(Food food){}
+    private final String DBURL = "jdbc:mysql://localhost:3306/food_inventory";
+    private final String USERNAME = "student";
+    private final String PASSWORD = "ensf";    
+    private Connection dbConnect;
+    private ResultSet results;
+
+
+    private ArrayList<Food> currentFood = new ArrayList<>();
+
+    public FoodList(){
+        try {
+            dbConnect = DriverManager.getConnection(this.DBURL, this.USERNAME, this.PASSWORD);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        String query = "SELECT * from available_food";
+
+        try {
+            Statement stmt = dbConnect.createStatement();
+            results = stmt.executeQuery(query);
+            while (results.next()) {
+                int itemID = results.getInt("ItemID");
+                String name = results.getString("Name");
+                int grainContent = results.getInt("GrainContent");
+                int fvContent = results.getInt("FVContent");
+                int proContent = results.getInt("ProContent");
+                int other = results.getInt("Other");
+                int calories = results.getInt("Calories");
+                Food food = new Food(itemID, name, grainContent, fvContent, proContent, other, calories);
+                currentFood.add(food);
+            }
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public void addFood(Food food){
+        
+    }
 
     public void removeFood(int foodID){}
 
@@ -20,5 +66,14 @@ public class FoodList {
 
     public void setFoodList(ArrayList<Food> foodList) throws IllegalArgumentException {
         this.currentFood = foodList;
+    }
+
+    public void close() {
+        try {
+            results.close();    
+            dbConnect.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
