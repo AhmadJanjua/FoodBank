00974e6b852ab4381b7bcd32ec2224a8c80f9b9e
AhmadJanjua <100788678+AhmadJanjua@users.noreply.github.com>
2022-04-16 16:57:59 -0600

    Added documentation to ClientList

--- edu/ucalgary/ensf409/ClientList.java
+++ edu/ucalgary/ensf409/ClientList.java
@@ -1,7 +1,7 @@
 package edu.ucalgary.ensf409;
 /**
 @author Ahmad Janjua, Zohaib
-@version 1.3
+@version 1.4
 @since 1.0
 */
 import java.util.*;
@@ -14,6 +14,7 @@ from the database it then gets gathers info related to their daily needs.
 
 public class ClientList {
 
+    //Fields to connect to the database
     private final String DBURL = "jdbc:mysql://localhost:3306/food_inventory";
     private final String USERNAME = "student";
     private final String PASSWORD = "ensf";    
@@ -27,8 +28,7 @@ public class ClientList {
             ex.printStackTrace();
         }
     }
-
-
+    //Fields for the ClientList
     private int numMale, numFemale, under, over;
     private ArrayList<Client> clients = new ArrayList<>();
     private double totalCalories= 0;
@@ -37,7 +37,16 @@ public class ClientList {
     private double totalProteinCalories = 0;
     private double totalOtherCalories = 0;
     private boolean isMobilityStruggled = false;    
-
+    /**
+     * Constructor:
+     * Generates a list of the requirements for the clients in the clientList
+     * and gets their needs from the database.
+     * @param numMale the number of male clients
+     * @param numFemale number of female clients
+     * @param under number of children under 8
+     * @param over number of children over 8
+     * @param isMobilityStruggled Do the clients need accomodation
+     */
     public ClientList(int numMale, int numFemale, int under, int over, boolean isMobilityStruggled) {
         this.numMale = numMale;
         this.numFemale = numFemale;
@@ -95,10 +104,10 @@ public class ClientList {
         }
         this.setNutrientNeeds();
     }
-    
-    public ArrayList<Client> getClients() {
-        return clients;
-    }
+    /**
+     * Using the current client list it sets the needs of the nutrient needs
+     * of the clients
+     */
     public void setNutrientNeeds() {
         this.totalCalories = 0; 
         this.totalGrainCalories = 0;
@@ -114,60 +123,30 @@ public class ClientList {
             this.totalOtherCalories += (0.01)*tmp.getOtherPercent()*tmp.getCalories();
         }
     }
-    //Returns a String of clients
+    /**
+    * 
+    * @return A list of the clients by their type and quantity.
+    */
     public String getClientString() {
-            String clientString = "";
-            if(numMale != 0){
-                clientString += numMale + " Adult Male, ";
-            }
-            if(numFemale != 0){
-                clientString += numFemale + " Adult Female, ";
-            }
-            if(under != 0){
-                clientString += under + " Child under 8, ";
-            }
-            if(over != 0){
-                clientString += over + " Child over 8, ";
-            }
-            return clientString.substring(0, clientString.length()-2);
-    
+        String clientString = "";
+        if(numMale != 0){
+            clientString += numMale + " Adult Male, ";
         }
-    public double getTotalGrainCalories() {
-        return totalGrainCalories;
+        if(numFemale != 0){
+            clientString += numFemale + " Adult Female, ";
+        }
+        if(under != 0){
+            clientString += under + " Child under 8, ";
+        }
+        if(over != 0){
+            clientString += over + " Child over 8, ";
+        }
+        return clientString.substring(0, clientString.length()-2);
     }
-    public void setTotalGrainCalories(int totalGrainCalories) {
-        this.totalGrainCalories = totalGrainCalories;
-    }
-    public double getTotalFVCalories() {
-        return totalFVCalories;
-    }
-    public void setTotalFVCalories(int totalFVCalories) {
-        this.totalFVCalories = totalFVCalories;
-    }
-    public double getTotalProteinCalories() {
-        return totalProteinCalories;
-    }
-    public void setTotalProteinCalories(int totalProteinCalories) {
-        this.totalProteinCalories = totalProteinCalories;
-    }
-    public double getTotalOtherCalories() {
-        return totalOtherCalories;
-    }
-    public void setTotalOtherCalories(int totalOtherCalories) {
-        this.totalOtherCalories = totalOtherCalories;
-    }
-    public double getTotalCalories() {
-        return totalCalories;
-    }
-    public void setTotalCalories(int totalCalories) {
-        this.totalCalories = totalCalories;
-    }
-    public boolean isMobilityStruggled() {
-        return isMobilityStruggled;
-    }
-    public void setMobilityStruggled(boolean isMobilityStruggled) {
-        this.isMobilityStruggled = isMobilityStruggled;
-    }
+    /**
+     * Removes a client that is specified
+     * @param client a valid client type. ie: "child under 8"
+     */
     public void removeClient(String client) {
         for(int i= 0; i<clients.size(); i++){
             if(clients.get(i).getClientType().equals(client)){
@@ -177,4 +156,45 @@ public class ClientList {
         }
         this.setNutrientNeeds();
     }
+    //Getters for fields
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+    public double getTotalGrainCalories() {
+        return totalGrainCalories;
+    }
+    public double getTotalFVCalories() {
+        return totalFVCalories;
+    }
+    public double getTotalProteinCalories() {
+        return totalProteinCalories;
+    }
+    public double getTotalOtherCalories() {
+        return totalOtherCalories;
+    }
+    public double getTotalCalories() {
+        return totalCalories;
+    }    public boolean isMobilityStruggled() {
+        return isMobilityStruggled;
+    }
+    //Setters for fields
+    public void setTotalGrainCalories(int totalGrainCalories) {
+        this.totalGrainCalories = totalGrainCalories;
+    }
+    public void setTotalFVCalories(int totalFVCalories) {
+        this.totalFVCalories = totalFVCalories;
+    }
+    public void setTotalProteinCalories(int totalProteinCalories) {
+        this.totalProteinCalories = totalProteinCalories;
+    }
+    public void setTotalOtherCalories(int totalOtherCalories) {
+        this.totalOtherCalories = totalOtherCalories;
+    }
+    public void setTotalCalories(int totalCalories) {
+        this.totalCalories = totalCalories;
+    }
+    public void setMobilityStruggled(boolean isMobilityStruggled) {
+        this.isMobilityStruggled = isMobilityStruggled;
+    }
+
 }
