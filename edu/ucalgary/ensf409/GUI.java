ea85edea33cbee235888927135dc501da69eb04e
Orionahn <101444254+Orionahn@users.noreply.github.com>
2022-04-16 01:34:27 -0600

    More Changes to GUI + Order Format

--- edu/ucalgary/ensf409/GUI.java
+++ edu/ucalgary/ensf409/GUI.java
@@ -1,6 +1,5 @@
 package edu.ucalgary.ensf409;
 
-
 /**
 @author Pedro Ghodsi
 @version 1.1
@@ -24,120 +23,128 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.EventQueue;
 import java.util.ArrayList;
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;    
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
- 
-
 
 public class GUI extends JPanel {
-    private JLabel hamperCreatorLabel;
+    public static JLabel hamperCreatorLabel;
+    public static JLabel nameLabel;
+    public static JTextField name;
     public static JCheckBox mobReqBox;
-    private JLabel adultMaleLabel;
-    private JTextField adultMaleBox;
-    private JLabel adultFemaleLabel;
-    private JTextField adultFemaleBox;
-    private JLabel over8;
-    private JTextField over8Box;
-    private JLabel under8;
-    private JTextField under8Box;
-    private JLabel postCode;
-    private JTextField postCodeBox;
-    private JButton continueHamperButton;
-    private JButton finalizeHamperButton;
+    public static JLabel adultMaleLabel;
+    public static JTextField adultMaleBox;
+    public static JLabel adultFemaleLabel;
+    public static JTextField adultFemaleBox;
+    public static JLabel over8;
+    public static JTextField over8Box;
+    public static JLabel under8;
+    public static JTextField under8Box;
+    public static JLabel postCode;
+    public static JTextField postCodeBox;
+    public static JButton continueHamperButton;
+    public static JButton finalizeHamperButton;
+    public static int counting;
 
     public GUI() {
         initComponents();
-        
+
     }
 
-    class ClickListener implements ActionListener{
-        public void actionPerformed(ActionEvent e) { 
-            if (e.getSource() == continueHamperButton){
-                Order order = new Order();
-                try {
-                    order.orderCreation();
-                } catch (InsufficientFoodException e1) {
-                    e1.printStackTrace();
-                } catch (InsufficientStockException e1) {
-                    e1.printStackTrace();
+    class ClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            Order order = new Order();
+            counting++;
+            if (counting == 1) {
+                if (e.getSource() == continueHamperButton) {
+                    try {
+                        order.addFirstOrder();
+                    } catch (InsufficientFoodException e2) {
+                        e2.printStackTrace();
+                    }
+                }
+            } else {
+
+                if (e.getSource() == continueHamperButton) {
+                    try {
+                        order.orderCreation();
+                    } catch (InsufficientFoodException e1) {
+                        e1.printStackTrace();
+                    } catch (InsufficientStockException e1) {
+                        e1.printStackTrace();
+                    }
                 }
             }
         }
     }
 
-    class ClickListenerTwo implements ActionListener{
-        public void actionPerformed(ActionEvent e){
-            if(e.getSource() == finalizeHamperButton){
+    class ClickListenerTwo implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == finalizeHamperButton) {
                 System.exit(1);
             }
         }
     }
 
-    public void initComponents(){
-        adultMaleLabel = new JLabel ("Adult Males:");
-        adultMaleBox = new JTextField (5);
-        adultFemaleLabel = new JLabel ("Adult Females:");
-        adultFemaleBox = new JTextField (5);
-        over8 = new JLabel ("Over 8:");
-        over8Box = new JTextField (5);
-        under8 = new JLabel ("Under 8:");
-        under8Box = new JTextField (5);
-        postCode = new JLabel ("Postal Code (ex. T2N 1N4)");
-        postCodeBox = new JTextField (5);
-        continueHamperButton = new JButton ("Add Current Hamper");
-        finalizeHamperButton = new JButton ("Finalize All Hampers");
-        hamperCreatorLabel = new JLabel ("Hamper Creator");
-        mobReqBox = new JCheckBox ("Mobility Accomodations Required");
+    public void initComponents() {
+        nameLabel = new JLabel("Name:");
+        name = new JTextField(5);
+        adultMaleLabel = new JLabel("Adult Males:");
+        adultMaleBox = new JTextField(5);
+        adultFemaleLabel = new JLabel("Adult Females:");
+        adultFemaleBox = new JTextField(5);
+        over8 = new JLabel("Over 8:");
+        over8Box = new JTextField(5);
+        under8 = new JLabel("Under 8:");
+        under8Box = new JTextField(5);
+        postCode = new JLabel("Postal Code (ex. T2N 1N4)");
+        postCodeBox = new JTextField(5);
+        continueHamperButton = new JButton("Add Current Hamper");
+        finalizeHamperButton = new JButton("Finalize All Hampers");
+        hamperCreatorLabel = new JLabel("Hamper Creator");
+        mobReqBox = new JCheckBox("Mobility Accomodations Required");
         ClickListener click = new ClickListener();
         continueHamperButton.addActionListener(click);
         ClickListenerTwo clickEnd = new ClickListenerTwo();
         finalizeHamperButton.addActionListener(clickEnd);
 
-    
-        
-        //adjust size and set layout
-        setPreferredSize (new Dimension (683, 334));
-        BoxLayout layout = new BoxLayout (this, BoxLayout.Y_AXIS);
-        setLayout (layout);
-        
-        
-        //add components
-        add (hamperCreatorLabel);
-        add (mobReqBox);
-        add (adultMaleLabel);
-        add (adultMaleBox);
-        add (adultFemaleLabel);
-        add (adultFemaleBox);
-        add (over8);
-        add (over8Box);
-        add (under8);
-        add (under8Box);
-        add (postCode);
-        add (postCodeBox);
-        add (continueHamperButton);
-        add (finalizeHamperButton);
+        // adjust size and set layout
+        setPreferredSize(new Dimension(683, 334));
+        BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        setLayout(layout);
+
+        // add components
+        add(hamperCreatorLabel);
+        add(nameLabel);
+        add(name);
+        add(adultMaleLabel);
+        add(adultMaleBox);
+        add(adultFemaleLabel);
+        add(adultFemaleBox);
+        add(over8);
+        add(over8Box);
+        add(under8);
+        add(under8Box);
+        add(postCode);
+        add(postCodeBox);
+        add(mobReqBox);
+        add(continueHamperButton);
+        add(finalizeHamperButton);
     }
 
-
     public static void main(String[] args) throws InsufficientFoodException, InsufficientStockException {
-        
-        JFrame frame = new JFrame ("GUI");
-        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add (new GUI());
+
+        JFrame frame = new JFrame("GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new GUI());
         frame.pack();
-        frame.setVisible (true);
-        //boolean inNeed = MobilityStruggling();
-        
+        frame.setVisible(true);
+        // boolean inNeed = MobilityStruggling();
 
-    
-        
-}
+    }
 
 }
-
-
--- edu/ucalgary/ensf409/Order.java
+++ edu/ucalgary/ensf409/Order.java
@@ -15,31 +15,35 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import edu.ucalgary.ensf409.GUI.ClickListener;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Order {
     
+
     public void orderCreation() throws InsufficientFoodException, InsufficientStockException{
-    ClientList cList = new ClientList(1, 1, 1, 1, GUI.mobReqBox.isSelected()); // to be fixed with GUI later
+        ClientList cList = new ClientList(1, 1, 1, 1, GUI.mobReqBox.isSelected()); // to be fixed with GUI later
+        GUI needCounter = new GUI();
+
         //System.out.println(cList.getClientString());   ADDED to Check if it prints out the clientlist properly
         FoodList fList = new FoodList();
-        String nameInput = "Name:" + ""; // to be fixed with gui later
-        DateTimeFormatter today = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        int hamperNumber = 1;
-        LocalDateTime now = LocalDateTime.now();
         fList.fillFromDatabase();
         Hamper hamper = new Hamper(cList, fList);
-        System.out.println(hamper.createOrderFormat());
+        //System.out.println(hamper.createOrderFormat());
         try
         {
             FileWriter fileWriter = new FileWriter("ExampleOutputUnfinished.txt", true);
-            fileWriter.append(nameInput + "\n");
-            fileWriter.append("Date: " +today.format(now) + "\n" + "\n");
             if(GUI.mobReqBox.isSelected() == true){
-                fileWriter.append("Mobility Accomodations Requested, Delivery is Available\n");
+                fileWriter.append("Mobility Accomodations Requested, Hamper will be delivered to the address associated with the Postal Code\n");
+                fileWriter.append("Your Postal Code: " + GUI.postCodeBox.getText() + "\n");
             }
             fileWriter.append("Original Request" +"\n");
-            fileWriter.append("Hamper " + hamperNumber + ": " + cList.getClientString() + "\n\n" );
-            fileWriter.append("Hamper " + hamperNumber +" Items:\n");
-            fileWriter.append(hamper.createOrderFormat());
+            fileWriter.append("Hamper " + needCounter.counting + ": " + cList.getClientString() + "\n\n" );
+            fileWriter.append("Hamper " + needCounter.counting +" Items:\n");
+            fileWriter.append(hamper.createOrderFormat() + "\n\n");
             
             fileWriter.flush();
             fileWriter.close();
@@ -49,6 +53,41 @@ public class Order {
             ioException.printStackTrace();
         }
     }
+
+    public void addFirstOrder() throws InsufficientFoodException{
+        ClientList cList = new ClientList(1, 1, 1, 1, GUI.mobReqBox.isSelected()); // to be fixed with GUI later
+        //System.out.println(cList.getClientString());   ADDED to Check if it prints out the clientlist properly
+        FoodList fList = new FoodList();
+        int hamperNumber = 1;
+        fList.fillFromDatabase();
+        Hamper hamper = new Hamper(cList, fList);
+        
+        String nameInput = "Name:" + GUI.name.getText(); // to be fixed with gui later
+        try{
+            FileWriter headerWriter = new FileWriter("ExampleOutputUnfinished.txt", true);
+            headerWriter.append(nameInput + "\n");
+            DateTimeFormatter today = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            headerWriter.append("Date: " +today.format(now) + "\n" + "\n");
+            if(GUI.mobReqBox.isSelected() == true){
+                headerWriter.append("Mobility Accomodations Requested, Hamper will be delivered to the address associated with the Postal Code\n");
+                headerWriter.append("Your Postal Code: " + GUI.postCodeBox.getText() + "\n");
+            }
+            headerWriter.append("Original Request" +"\n");
+            headerWriter.append("Hamper " + hamperNumber + ": " + cList.getClientString() + "\n\n" );
+            headerWriter.append("Hamper " + hamperNumber +" Items:\n");
+            headerWriter.append(hamper.createOrderFormat() + "\n\n");
+            headerWriter.flush();
+            headerWriter.close();
+        }
+        catch(IOException ioException)
+        {
+            ioException.printStackTrace();
+        }
+    }
+
+
+
     
     
     
