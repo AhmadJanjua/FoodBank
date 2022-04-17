d99d9eb0370103b5dcba73a1c11624811bd4fb74
zohaib2001 <102832419+zohaib2001@users.noreply.github.com>
2022-04-17 12:17:02 -0600

    Completed

---
+++ edu/ucalgary/ensf409/InsufficientStockExceptionTest.java
@@ -0,0 +1,23 @@
+package edu.ucalgary.ensf409;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * ENSF 409 Final Project Group 7
+ * @author Ahmad Janjua, Pedro Ghodsi, Zohaib Ashfaq, Farica Mago
+ * @version 1.1
+ * @since 1.0
+ */
+
+public class InsufficientStockExceptionTest {
+        @Test
+        public void InsufficientStockExceptionTest() throws CustomeException {
+            if (super(errormessage)) {
+                throw new CustomeException(<"There are not enough food items to satisfy that order, try again.",
+                "Not Enough Food">)
+            }
+        }
+
+
+}
